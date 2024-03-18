10004aa21bf57e1c9c4ada9c346f68f59c2dd105
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2024-03-18 10:04:26 +0300

    variables & datatypes

---
+++ datatypes.js
@@ -0,0 +1,32 @@
+//
+
+// 1. Strings
+const firstName = 'Jane';
+const lastName = 'Doe';
+const fullName = `Jane Doe`;
+
+console.log(typeof firstName);
+console.log(typeof fullName);
+
+// string concatenation using + operator
+console.log(firstName + ' ' + lastName);
+
+// curly brackets -> {}
+// paranthesis (soft brackets) -> ()
+// square brackets -> []
+
+// string interpolation - injecting values into strings
+console.log(`${firstName} ${lastName}, age is ${100 - 45}`);
+console.log('${firstName} ${lastName}, age is ${100 - 45}');
+
+// 2. Numbers
+
+// 3. boolean
+
+// 4. objects (arrays)
+
+// 5. null
+
+// 6. undefined
+
+// 7. symbols
---
+++ variables.js
@@ -0,0 +1,33 @@
+// Variable - a container in which we can store values for later retrieval
+
+/** Rules when creating variables in JS
+ * 1. Use camelCase naming convention
+ * 2. Case senstitive (all variables should start with a small letter)
+ * 3. We should not use keywords (reserved words for a programming language)
+ * 4. Should not start with a number
+ */
+
+// 1. Immutable variables using const
+// declaring a variable
+const fullName = 'Jane Doe';
+
+console.log(fullName);
+
+// fullName = 'Allan';
+
+// console.log(fullName);
+
+// fullName = 'Willis';
+
+// console.log(fullName);
+
+// 2. Mutable variables using let
+// declaring
+let age = 30;
+
+console.log(age);
+
+// reassign
+age = 34;
+
+console.log(age);
